34dd2a7d3da84d45cb865e40e9d3271560945541
Harshil Gajera <hgajera@splunk.com>
2020-09-18 14:27:54 +0530

    testing3 user_access.py

--- examples/test_user_access.py
+++ examples/test_user_access.py
@@ -32,8 +32,8 @@ def test_object_acl_manager():
                      replace_existing=False)
     obj_acl = oaclm.get_acl(obj_collection, obj_id1)
     assert (type((obj_acl.obj_perms['read']) == list), f"{obj_acl.obj_perms['read']}"
-    # assert (obj_acl.obj_perms['read']).sort() == ['admin', 'user1']
 
+    assert set((obj_acl.obj_perms['read'])) == set(['admin', 'user1'])
     oaclm.update_acls(obj_collection, [obj_id2, obj_id3], obj_type,
                       context.app, context.owner, obj_perms1, True)
     oaclm.get_acl(obj_collection, obj_id2)
